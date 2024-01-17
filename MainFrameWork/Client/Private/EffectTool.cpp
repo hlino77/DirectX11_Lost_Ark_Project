754eb31d011e152e0401e6b860280d3ed6779ffc
DESKTOP-NPLG6RC\star1 <ksg7186@gmail.com>
2024-01-17 19:27:46 +0900

    feature ksg : 디스토션 툴 수정

--- MainFrameWork/Client/Private/EffectTool.cpp
+++ MainFrameWork/Client/Private/EffectTool.cpp
@@ -499,6 +499,7 @@ HRESULT CEffectTool::EffectDetail()
 
 	ImGui::InputFloat("Bloom Intensity", &m_pCurrentEffect->m_Intensity.fBloom);
 	ImGui::InputFloat("Distortion Intensity", &m_pCurrentEffect->m_Intensity.fDistortion);
+	ImGui::Checkbox("Distortion On Base Material", (_bool*)&m_pCurrentEffect->m_Intensity.iDistortionOnBaseMaterial);
 
 	ImGui::InputFloat2("UV Speed", (_float*)&m_pCurrentEffect->m_vUV_Speed, "%.7f");
 
@@ -989,6 +990,7 @@ HRESULT CEffectTool::Save(_char* szGroupName)
 
 			element = document->NewElement("Distortion");
 			element->SetAttribute("Intensity", m_vecEffects[i]->m_Intensity.fDistortion);
+			element->SetAttribute("OnBaseMaterial", m_vecEffects[i]->m_Intensity.iDistortionOnBaseMaterial);
 			node->LinkEndChild(element);
 			
 			element = document->NewElement("Dissolve");
@@ -1269,6 +1271,7 @@ HRESULT CEffectTool::Load()
 
 			element = element->NextSiblingElement();
 			m_pCurrentEffect->m_Intensity.fDistortion = element->FloatAttribute("Intensity");
+			m_pCurrentEffect->m_Intensity.iDistortionOnBaseMaterial = element->IntAttribute("OnBaseMaterial");
 
 			element = element->NextSiblingElement();
 			m_pCurrentEffect->m_Intensity.fDissolveAmount = element->FloatAttribute("Amount");
