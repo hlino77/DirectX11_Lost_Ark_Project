ed5cbb423d85b9067f052a5216ec951fa6eeefdb
DESKTOP-NPLG6RC\star1 <ksg7186@gmail.com>
2024-02-02 21:28:44 +0900

    feature ksg : 겸사겸사 이거 추가.

--- MainFrameWork/Engine/Private/GameInstance.cpp
+++ MainFrameWork/Engine/Private/GameInstance.cpp
@@ -134,14 +134,30 @@ void CGameInstance::Tick(_float fTimeDelta)
 	m_pPhysXMgr->LateTick(fTimeDelta);
 	m_pLevel_Manager->LateTick(fTimeDelta);
 
-	if (KEY_HOLD(KEY::ALT) && KEY_TAP(KEY::PAGE_UP))
-		2 <= m_iIBL_Index_Start ? m_iIBL_Index_Start = 2 : ++m_iIBL_Index_Start;
-	else if (KEY_HOLD(KEY::ALT) && KEY_TAP(KEY::PAGE_DOWN))
-		0 >= m_iIBL_Index_Start ? m_iIBL_Index_Start = 0 : --m_iIBL_Index_Start;
-
-	for (_int i = 0; i < 9; ++i)
-		if (KEY_HOLD(KEY::ALT) && KEY_TAP(KEY(22 + i)))
-			CRenderer::Set_IBLTexture(10 * m_iIBL_Index_Start + i);
+	if (KEY_HOLD(KEY::ALT) && KEY_TAP(KEY::UP_ARROW))
+	{
+		if (2 > m_iIBL_Index / 10) m_iIBL_Index += 10;
+		CRenderer::Set_IBLTexture(m_iIBL_Index);
+		cout << "IBL Index : " << m_iIBL_Index << '\n';
+	}
+	else if (KEY_HOLD(KEY::ALT) && KEY_TAP(KEY::DOWN_ARROW))
+	{
+		if (0 < m_iIBL_Index / 10) m_iIBL_Index -= 10;
+		CRenderer::Set_IBLTexture(m_iIBL_Index);
+		cout << "IBL Index : " << m_iIBL_Index << '\n';
+	}
+	if (KEY_HOLD(KEY::ALT) && KEY_TAP(KEY::RIGHT_ARROW))
+	{
+		22 > m_iIBL_Index ? ++m_iIBL_Index : m_iIBL_Index = 22;
+		CRenderer::Set_IBLTexture(m_iIBL_Index);
+		cout << "IBL Index : " << m_iIBL_Index << '\n';
+	}
+	else if (KEY_HOLD(KEY::ALT) && KEY_TAP(KEY::LEFT_ARROW))
+	{
+		0 < m_iIBL_Index ? --m_iIBL_Index : m_iIBL_Index = 0;
+		CRenderer::Set_IBLTexture(m_iIBL_Index);
+		cout << "IBL Index : " << m_iIBL_Index << '\n';
+	}
 }
 
 void CGameInstance::FinalTick(_float fTimeDelta)
